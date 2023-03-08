d3c97cb323249913fe334cbd4cf53169ca9e4dbd
Ragulprabhu196 <123489882+Ragulprabhu196@users.noreply.github.com>
2023-03-08 21:17:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-const Templateuser = document.querySelector("[data-user-template]")
-const usercardcontainer = document.querySelector("[data-user-cards-container]")
-const searchinput = document.querySelector("[data-search]")
+const uri = "https://api.openbrewerydb.org/breweries";
+const countriesContainer = document.getElementById("persondetailscontainer");
+const searchInput = document.getElementById("search");
+let countries = [];
 
-let users = []
+function handleSearch(target) {
+  const search = target.value.toLowerCase();
+  const searchMatch = countries.filter((element) => {
+    const name = element.name.common.toLowerCase();
+    return name.includes(search);
+  });
+  renderCards(searchMatch);
+}
 
-searchinput.addEventListener("input",(e)=>{
-    const value =e.target.value.toLowercase()
-    users.forEach(user=>{
-        const isvisible =user.name.toLowercase().includes(value)||user.email.toLowercase().includes(value)
-        user.element.classList.toggle('hide',!isvisible)
-
-    })
-})
-
-
-fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json()).then(data =>{
-    users=data.map(user => {
-        
-    
-    const card = Templateuser.content.cloneNode(true).children[0]
-    const header=card.querySelector("[data-header]")
-    const body = card.querySelector("[data-body]")
-    header.textcontent = user.name
-    body.textcontent = user.email
-    usercardcontainer.append(card)
-
-    return{
-        name:user.name, email:user.email, element:card
-    }
+async function fetchData() {
+  const response = await fetch(uri);
+  const data = await response.json();
+  if (data.length > 0) {
+    countries = [...data];
+    renderCards(countries);
+  }
+}
+fetchData();
+function renderCards(data = []) {
   
-    })
-})
+  let cards = [];
+  for (let i = 0; i < data.length; i++) {
+    cards.push(createCard(data[i]));
+  }
+  
+  countriesContainer.innerHTML = "";
+  
+  countriesContainer.append(...cards);
+}
+
+function createCard(data = {}) {
+  let card = document.createElement("div");
+  let title = document.createElement("h2");
+  let subHeading = document.createElement("p");
+
+  card.setAttribute("class", "card");
+
+ 
+  const { name = "", latitude = "" } = data;
+  title.innerText = name;
+  subHeading.innerText = latitude;
+ 
+  card.append(title, subHeading);
+
+  return card;
+}
